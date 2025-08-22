8b2140b9b473296d4794d86c60f2cfe305a8cba0
Bryan0324 <34998621+Bryan0324@users.noreply.github.com>
2025-08-22 21:38:59 +0800

    修改檔案位置

--- src/pages/main-screen/BannerMarquee.jsx
+++ src/pages/main-screen/BannerMarquee.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import Marquee from "react-fast-marquee";
 import "./BannerMarquee.css";
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
-const banners = [
-  require("./img/main_screen/news_ticker/114_test_time.png")
-];
 
 export default function BannerMarquee() {
   return (
     <Marquee gradient={false} speed={50}>
-      {banners.map((src, index) => (
-        <img key={index} src={src} alt={`Banner ${index}`} className="banner-img" />
-      ))}
+      <img src={useBaseUrl("/img/main_screen/news_ticker/114_test_time.png")} className="banner-img" />
     </Marquee>
   );
 }
---
+++ src/pages/main-screen/MenuGrid.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./MenuGrid.css";
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
+const menuItems = [
+  { link: "/img/main_screen", img: "/img/main_screen/1.png" },
+  { link: "/page2", img: "/img/main_screen/2.png" },
+  { link: "/page3", img: "/img/main_screen/3.png" },
+  { link: "/page4", img: "/img/main_screen/4.png" },
+  { link: "/page5", img: "/img/main_screen/5.png" },
+  { link: "/page6", img: "/img/main_screen/6.png" },
+  { link: "/page7", img: "/img/main_screen/7.png" },
+  { link: "/page8", img: "/img/main_screen/8.png" },
+];
+
+export default function MenuGrid() {
+  return (
+    <div className="menu-grid">
+      {menuItems.map((item, index) => (
+        <a key={index} href={item.link} className="menu-item">
+          <img src={useBaseUrl(item.img)} alt={`選單圖片 ${index + 1}`} />
+        </a>
+      ))}
+    </div>
+  );
+}
