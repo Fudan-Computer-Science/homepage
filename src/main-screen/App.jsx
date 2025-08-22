e25995838a89504579d910ca43aeb366685fc6ab
Miyun <miyun200905270527@gmail.com>
2025-08-22 19:56:28 +0800

    我終於修好了好感動

---
+++ src/main-screen/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BannerMarquee from "./BannerMarquee";
+import MenuGrid from "./MenuGrid";
+
+export default function App() {
+  return (
+    <div>
+      <BannerMarquee />
+      <MenuGrid />
+    </div>
+  );
+}
