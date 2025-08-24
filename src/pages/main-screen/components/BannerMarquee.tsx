359d9f3e6496113e115bdfac4d7c869c942362a9
Bryan0324 <34998621+Bryan0324@users.noreply.github.com>
2025-08-25 05:04:44 +0800

    修正細節

--- src/pages/main-screen/components/BannerMarquee.tsx
+++ src/pages/main-screen/components/BannerMarquee.tsx
@@ -19,34 +19,32 @@ export default function BannerMarquee() {
     <header className={clsx("hero", styles.heroBanner)}>
       {/* 背景跑馬燈 */}
       <div className={styles.marqueeBg}>
-<Marquee speed={50} gradient={false}>
-  {[...banners, ...banners].map((link, idx) => (
-    <div
-      key={idx}
-      style={{
-        height: 400,     // Banner 高度
-        flexShrink: 0,
-        overflow: "hidden",
-        position: "relative",
-        //width: 600       // 每張圖片寬度，可調整
-      }}
-    >
-      <img
-        src={useBaseUrl(link)}
-        style={{
-          width: "100%",
-          height: "100%",
-          objectFit: "cover"
-        }}
-        alt="banner"
-      />
-    </div>
-  ))}
-</Marquee>
+        <Marquee speed={50} gradient={false}>
+          {[...banners, ...banners].map((link, idx) => (
+            <div
+              key={idx}
+              style={{
+                height: 400,     // Banner 高度
+                flexShrink: 0,
+                overflow: "hidden",
+                position: "relative",
+                //width: 600       // 每張圖片寬度，可調整
+              }}
+            >
+              <img
+                src={useBaseUrl(link)}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover"
+                }}
+                alt="banner"
+              />
+            </div>
+          ))}
+        </Marquee>
       </div>
 
-      {/* 漸層遮罩 */}
-      <div className={styles.gradientOverlay}></div>
 
       {/* 文字內容 */}
       <div className={styles.textOverlay}>
@@ -54,13 +52,14 @@ export default function BannerMarquee() {
           {siteConfig.title}
         </Heading>
         <p className={styles.subtitle}>{siteConfig.tagline}</p>
-{/*         <Link
-          className="button button--secondary button--lg"
-          to="/docs/intro/intro/入班考試資訊"
-        >
-          入班考資訊
-        </Link> */}
       </div>
     </header>
   );
 }
+
+/*         <Link
+          className="button button--secondary button--lg"
+          to="/docs/intro/intro/入班考試資訊"
+        >
+          入班考資訊
+        </Link> */
