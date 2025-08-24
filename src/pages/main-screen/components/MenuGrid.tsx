6c22afea0e3bb792233e6d27ea503b5a9a03d1c9
Bryan0324 <34998621+Bryan0324@users.noreply.github.com>
2025-08-24 14:50:01 +0800

    update backgroundColor

--- src/pages/main-screen/components/MenuGrid.tsx
+++ src/pages/main-screen/components/MenuGrid.tsx
@@ -36,7 +36,7 @@ const theme:ColorSet[] = [
 ]
 export default function CardGrid(): React.ReactElement {
   const [maxCols, setMaxCols] = useState(4);
-  const [screenW, setScreenW] = useState(0);
+  const [screenW, setScreenW] = useState(1200);
 
   useEffect(() => {
     function updateGrid() {
