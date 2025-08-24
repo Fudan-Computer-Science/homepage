b30afe3ba449d4656f5bc1cf286156f732382285
Miyun <miyun200905270527@gmail.com>
2025-08-24 20:06:39 +0800

    把新版主頁面做完大部分了，但圖好像被吃掉

--- src/pages/main-screen/components/MenuGrid.tsx
+++ src/pages/main-screen/components/MenuGrid.tsx
@@ -14,16 +14,28 @@ type CardProps = {
 
 /* 此處新增主頁字卡 */
 import coding from "@site/static/img/main_screen/coding.svg";
+import a1_code from "@site/static/img/main_screen/a1_code.svg";
+import a2_register_now from "@site/static/img/main_screen/a2_register_now.svg";
+import a3_faq from "@site/static/img/main_screen/a3_faq.svg";
+import a4_blog from "@site/static/img/main_screen/a4_blog.svg";
+import a5_ddj from "@site/static/img/main_screen/a5_ddj.svg"; 
+import a6_intro_textbook from "@site/static/img/main_screen/a6_intro_textbook.svg";
+import a7_textbook from "@site/static/img/main_screen/a7_textbook.svg"; 
+import a8_contect_us from "@site/static/img/main_screen/a8_contect_us.svg";
+import a9_competition_result from "@site/static/img/main_screen/a9_competition_result.svg";
+import a10_bulletin_board from "@site/static/img/main_screen/a10_bulletin_board.svg";
 
 const cardsData: CardProps[] = [
-  { text: "關於程式設計班", link: "/homepage", img: coding},
-  { text: "加入我們", link: "/page2", img: coding},
-  { text: "FAQ", link: "/page3", img: coding},
-  { text: "部落格", link: "/page3", img: coding},
-  { text: "旦旦解題網", link: "/page3", img: coding},
-  { text: "入班考講義", link: "/page3", img: coding},
-  { text: "課堂講義", link: "/page3", img: coding},
-  { text: "聯絡我們", link: "/page3", img: coding},
+  { text: "關於程式設計班", link: "/homepage", img: a1_code},
+  { text: "加入我們", link: "/page2", img: a2_register_now},
+  { text: "FAQ", link: "/page3", img: a3_faq},
+  { text: "部落格", link: "/page3", img: a4_blog},
+  { text: "旦旦解題網", link: "/page3", img: a5_ddj},
+  { text: "入班考講義", link: "/page3", img: a6_intro_textbook},
+  { text: "課堂講義", link: "/page3", img: a7_textbook},
+  { text: "聯絡我們", link: "/page3", img: a8_contect_us},
+  {text: "競賽成果", link: "/page3", img: a9_competition_result},
+  {text: "公布欄", link: "/page3", img: a10_bulletin_board},
 ];
 
 const theme:ColorSet[] = [
