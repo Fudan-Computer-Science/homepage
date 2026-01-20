b6524da292b7df786a6f5265ce5929ea3a763fbb
Bryan0324 <34998621+Bryan0324@users.noreply.github.com>
2026-01-20 20:37:46 +0800

    feat: Add Algolia search configuration to theme settings

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -111,6 +111,41 @@ const config: Config = {
   ],
 
   themeConfig: {
+    algolia: {
+      // The application ID provided by Algolia
+      appId: 'P2A7UCAY5O',
+
+      // Public API key: it is safe to commit it
+      apiKey: '91b65f45f548721784506eb2ed5ca5de',
+
+      indexName: '復旦程設班官網',
+
+      // Optional: see doc section below
+      // contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      // externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      /* replaceSearchResultPathname: {
+        from: '/docs/', // or as RegExp: /\/docs\//
+        to: '/',
+      }, */
+
+      // Optional: Algolia search parameters
+      // searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      // searchPagePath: 'search',
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      // insights: false,
+
+      // Optional: whether you want to use the new Ask AI feature (undefined by default)
+      // askAi: 'YOUR_ALGOLIA_ASK_AI_ASSISTANT_ID',
+
+      //... other Algolia params
+    },
     giscus: {
       repo: 'Fudan-Computer-Science/homepage',
       repoId: 'R_kgDOOzYo6g',
