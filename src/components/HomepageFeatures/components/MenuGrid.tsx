3f8e7388c1bc78be694f42ac51bf51a98cd0834c
median <zhongweiliu927@gmail.com>
2026-09-01 22:03:15 +0800

    fixed/updated several hyperlinks

--- src/components/HomepageFeatures/components/MenuGrid.tsx
+++ src/components/HomepageFeatures/components/MenuGrid.tsx
@@ -32,7 +32,7 @@ import a10_bulletin_board from "@site/static/img/main_screen/A10BulletinBoard";
 
 const cardsData: CardProps[] = [
   { text: "關於程式設計班", link: "/homepage/docs/category/歷屆資訊/", img: A1Code},
-  { text: "加入我們", link: "homepage/docs/intro/intro/admision/入班考試資訊", img: a2_register_now},
+  { text: "加入我們", link: "/homepage/docs/category/招生專區/", img: a2_register_now},
   { text: "FAQ", link: "/docs/InGrid/FAQ", img: a3_faq},
   { text: "部落格", link: "/blog", img: a4_blog},
   { text: "ddj網站手冊", link: "/docs/category/ddj-v2-網站手冊", img: a5_ddj},
